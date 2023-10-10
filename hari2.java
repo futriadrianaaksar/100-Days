8512b8f1718fa90ce5317acc8993dd56bfe7f7c8
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-10-10 20:04:52 +0800

    hari2.java

---
+++ hari2.java
@@ -0,0 +1,18 @@
+package Pertemuan2;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String nama = "Futri";
+        System.out.println("nama saya \t\t: "+nama);
+        
+        String alamat = "Tapalang";
+        System.out.println("saya berdomisili di \t:"+alamat);
+        
+        String handphone = "085325792461";
+        System.out.println("info kontak \t\t: "+handphone);
+        
+        char kelas = 'A';
+        System.out.println(" dari kelas informatika\t: "+kelas);
+    }
+}
