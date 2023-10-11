d6cbe4086bd7de58ee6e6c57f0496ac81341b18c
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-10-11 19:01:23 +0800

    Futri3.java

---
+++ Futri3.java
@@ -0,0 +1,14 @@
+package pertemuan3;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int angka = 3;
+        int angka1 = 2;
+        int penjumlahan = angka + angka1;
+        System.out.println("hasil dari "+angka+" + "+angka1+" = "+penjumlahan);
+        
+        
+    }
+    
+}
