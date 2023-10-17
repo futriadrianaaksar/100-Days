7a8bf40fa7d1a63c7952ea87766177064ef13e62
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-10-17 08:34:09 +0800

    Day_9.java

---
+++ Day_9.java
@@ -0,0 +1,10 @@
+package Day9;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        double angka1 = 160.1;
+        System.out.println("tinggi badan saya " + angka1);
+    }
+    
+}
