f1863d970306b4091da90f880568b1000fe4218d
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-10-18 19:49:11 +0800

    Day_10.java

---
+++ Day_10.java
@@ -0,0 +1,10 @@
+package Day10;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        float angka1 = 1.5f;
+        System.out.println("contoh angka pecahan rendah adalah: "+ angka1);
+    }
+    
+}
