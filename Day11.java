0ee2500dd9bd826a6c30219b13612392dea42355
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-10-19 20:13:37 +0800

    Day11.java

---
+++ Day11.java
@@ -0,0 +1,10 @@
+package Day11;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        long angka1 = 12345L;
+        System.out.println("contoh nilai angka bulat adalah "+ angka1);
+    }
+    
+}
