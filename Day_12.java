265a2b540f7e4aa47ce6eac65a772e6847a03e8a
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-10-20 20:57:56 +0800

    Day_12.java

---
+++ Day_12.java
@@ -0,0 +1,13 @@
+package Day12;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        byte umur = 19;
+        byte umur1 = 9;
+        System.out.println("saya berumur " + umur + " tahun.");
+        System.out.println("sedangkan, adik saya berumur " + umur1 + " tahun.");
+        
+    }
+    
+}
