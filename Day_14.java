69736b5ee275ce86498ca005e84b941ea3d9cb60
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-10-22 21:24:50 +0800

    Day_14.java
    Menggunakan semua tipe data

---
+++ Day_14.java
@@ -0,0 +1,41 @@
+package Day14;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        // Tipe data int
+        int umur = 19;
+        
+        // Tipe data double
+        double suhu = 23.5;
+        
+        // Tipe data boolean
+        boolean benar = true;
+        
+        // Tipe data char
+        char grade = 'A';
+        
+        // Tipe data String
+        String nama = "Ana";
+        
+        // Tipe data float
+        float berat = 51.2f;
+        
+        // Tipe data long
+        long populasiKota = 1500000L;
+        
+        // Tipe data byte
+        byte angkaKecil = 7;
+        
+        
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur + " tahun");
+        System.out.println("Suhu: " + suhu + " derajat Celsius");
+        System.out.println("Benar? " + benar);
+        System.out.println("Grade: " + grade);
+        System.out.println("Berat: " + berat + " kg");
+        System.out.println("Populasi kota: " + populasiKota);
+        System.out.println("Angka kecil: " + angkaKecil);
+    }
+    
+}
