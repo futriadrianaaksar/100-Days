9b9ccc1f51afe5e8a2b84d6eed334f0b5a251b16
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-10-25 21:29:37 +0800

    Day_17.java

---
+++ Day_17.java
@@ -0,0 +1,17 @@
+package Day17;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String a,b,c;
+        a = "500";
+        b ="321";
+        c ="343";
+        
+        int d =Integer.parseInt(b);
+        double e = Double.parseDouble(a);
+        float f = Float.parseFloat(c);
+        System.out.println(d+"\n"+e+"\n"+f);
+    }
+    
+}
