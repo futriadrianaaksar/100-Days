96f7736de9b72a12a78616a813a865dd2571daad
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-10-27 19:37:43 +0800

    Day_19.java

---
+++ Day_19.java
@@ -0,0 +1,29 @@
+package Day19;
+
+public class Main {
+    
+    public static void main(String[] args) {
+            //Deklarasi variabel
+        String alamat,noWa;
+        byte saudara;
+        double ipk;
+
+
+        // inisialisasi variabel
+        String nama = "Futri";
+        byte umur = 19;
+        float  beratbadan = 50.1f;
+        double tinggibdn = 160.1;
+        char jenis = 'P';
+        System.out.println("hai guys Nama saya "+nama+"\nSaya berumur "+umur+"th"+"\nBerat badan saya "+beratbadan+"kg"+"\nTinggi "+tinggibdn+"cm"+"\nsaya berJenis kelamin "+jenis+"\n_______________________________");
+
+        //Update variabel
+        alamat = "Tapalang";
+        noWa = "085325792461";
+        saudara = 3;
+        ipk = 4.0;
+        System.out.println("No wa\t\t : "+noWa+"\nAlamat\t\t : "+alamat+"\nJumlah saudara\t : "+saudara+"\nber ipk\t\t : "+ipk);
+        System.out.println(":)");       
+    }
+    
+}
