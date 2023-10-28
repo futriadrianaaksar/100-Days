5cbb0e74f2a2f2e0aa4ed465b72de70861e372d5
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-10-28 19:06:38 +0800

    Day_20.java

---
+++ Day_20.java
@@ -0,0 +1,15 @@
+package Day20;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        final int harga_hp = 2500000;
+        final byte umur = 19 ;
+        final double bb = 50.1;
+        final float tb = 160.1f;
+        final short jarak = 45;
+
+        System.out.println ("Harga hpnya\t: "+harga_hp+"\nUmur\t\t: "+umur+"\nBerat badan \t: "+bb+"\nTinggi badan \t: "+tb+"\nJarak ke kampus\t: "+jarak+"km");
+    }
+    
+}
