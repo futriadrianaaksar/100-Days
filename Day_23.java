b04179dbfa521cb8ddb9ee9159489514d957504c
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-10-31 07:50:40 +0800

    Day_23.java
    operator penugasan

---
+++ Day_23.java
@@ -0,0 +1,20 @@
+package Day23;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int angka = 10;
+        
+        angka += 10; //penjumlahan
+        System.out.println(angka);
+        angka -= 5;  //pengurangan
+        System.out.println(angka);
+        angka *= 2;  //perkalian
+        System.out.println(angka);
+        angka /= angka; //pembagian
+        System.out.println(angka);
+        angka %= 1;  //sisa bagi
+        System.out.println(angka);
+    }
+    
+}
