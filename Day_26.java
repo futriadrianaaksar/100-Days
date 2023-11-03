cd309ec0322e6270dcd8172a3f8df3e1b4b1f2dd
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-11-03 11:00:59 +0800

    Day_26.java

---
+++ Day_26.java
@@ -0,0 +1,16 @@
+package Day26;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int umur = 20;
+
+        if (umur > 19) {
+        System.out.println("Anda sudah dewasa :)");
+        } else {
+        System.out.println("Anda masih di bawah umur.");
+        }
+
+    }
+    
+}
