42b8b9f9bdcf74f0b38acec9861cd15a84f1d8fb
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-11-05 21:19:06 +0800

    Day_28.java
    percabangan if else

---
+++ Day_28.java
@@ -0,0 +1,18 @@
+package hari28;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int umur = 20;
+
+        if (umur < 19) {
+        System.out.println("Anda sudah dewasa :)");
+        } else if(umur <= 20) {
+        System.out.println("Anda belum dewasa :)");
+        }else{
+            System.out.println("netral");
+        }
+
+    }
+    
+}
