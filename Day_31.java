38bf9f20f2f71bb8a95b3c586b08dd07d7427285
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-11-08 06:18:19 +0800

    Day_31.java

---
+++ Day_31.java
@@ -0,0 +1,15 @@
+package Day31;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int nomor = 1;
+        
+        //menggunakan perulangan for untuk mencetak nomor dari kecil ke besar
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Nomor: " + nomor);
+            nomor++; //melakukan peningkatan nomor setiap kali berjalan 
+        }
+    }
+    
+}
