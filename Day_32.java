21bf5e817dfcdf58b8f09a450d43ffba950a9f6a
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-11-09 20:49:57 +0800

    Day_32.java

---
+++ Day_32.java
@@ -0,0 +1,14 @@
+package Day32;
+
+public class Main {
+    
+    public static void main(String[] args) {
+         int nomor = 1;
+        
+        //menggunakan perulangan for untuk mencetak nomor dari besar ke kecil
+        for (int i = 10; i >= nomor; i--) {
+            System.out.println("Nomor: " + i);
+            //melakukan pngurangan nomor setiap kali berjalan 
+    }
+    }  
+}
