bfc3d4235a87d06f3e07993f64c812aaec1fa366
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-11-13 22:10:39 +0800

    Membuat persegi dengan perulangan

---
+++ Day_36.java
@@ -0,0 +1,14 @@
+package Hari36;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //membuat persegi dengan perulangan
+        for(int i = 0; i <= 6; i++){
+            for(int a = 0; a <= 6; a++){
+                System.out.print("* ");
+            }
+                System.out.println("");
+        }
+        }
+}
