afb4c7672eef6510a1b8ba42657a1b0d92472046
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-11-15 20:54:14 +0800

    Perulangan for

---
+++ Day_38.java
@@ -0,0 +1,13 @@
+package Day38;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        //menggunakan perulangan for untuk mencetak nomor dari kecil ke besar
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Nomor: " + i);
+    }
+    
+   }
+}
