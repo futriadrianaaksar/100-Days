b52435d188a03a5b57c1ae7ba5748b1fb77fe545
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-11-17 21:57:49 +0800

    Day_33.java

--- Day_33.java
+++ Day_33.java
@@ -5,11 +5,11 @@ public class Main {
     public static void main(String[] args) {
         
         int angka= 50;
-        //mulai dari angka nol sampai angka 50 akan di tambah dengan angka 2
+        //mulai dari angka nol sampai angka 50 akan di tambah dengan angka 5
         for (int i = 0; i < angka; i+=5) {
             System.out.println(i);
             }
-        //ketika sudah sampai mencapai angka 50, maka akan di kurangi dengan angka 2 lagi sampai angka 0
+        //ketika sudah sampai mencapai angka 50, maka akan di kurangi dengan angka 5 lagi sampai angka 0
                 for (int j= angka ; j > 0; j-=5) {
                     System.out.println(j);
                 }
