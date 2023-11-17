809a36aecb7ccd332e81a9f0d66180544ad87e21
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-11-17 18:53:25 +0800

    membuat persegi panjang dengan perulangan for

---
+++ Day_40.java
@@ -0,0 +1,16 @@
+package Day40;
+
+public class Main {
+    
+    public static void main(String[] args) {
+         //perulangan for
+         //membuat persegi panjang dengan perulangan
+            for(int i = 0; i <= 3; i++){ //luping ke bawah
+            for(int a = 0; a <= 6; a++){ //luping ke kanan
+                System.out.print("* ");
+            }
+                System.out.println("");
+        }
+    }
+    
+}
