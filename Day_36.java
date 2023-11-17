bead1500c9482aafc169b632dea2d82cdb2c2f73
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-11-17 19:07:05 +0800

    Membuat persegi dengan menggunakan perulangan bersarang

--- Day_36.java
+++ Day_36.java
@@ -3,9 +3,9 @@ package Hari36;
 public class Main {
     
     public static void main(String[] args) {
-        //membuat persegi dengan perulangan
-        for(int i = 0; i <= 6; i++){
-            for(int a = 0; a <= 6; a++){
+        //membuat persegi dengan menggunakan perulangan bersarang
+        for(int i = 0; i <= 6; i++){       //luping kebawah
+            for(int a = 0; a <= 6; a++){   //luping ke kanan
                 System.out.print("* ");
             }
                 System.out.println("");
