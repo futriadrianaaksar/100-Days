63d33a81811969b74a36bfd9225ba7c0d642143c
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-11-17 19:57:33 +0800

    Membuat persegi panjang dengan menggunakan perulangan bersarang

--- Day_40.java
+++ Day_40.java
@@ -5,12 +5,11 @@ public class Main {
     public static void main(String[] args) {
          
          //membuat persegi panjang dengan menggunakan perulangan bersarang
-            for(int i = 0; i <= 3; i++){ //luping ke bawah
+        for(int i = 0; i <= 3; i++){ //luping ke bawah
             for(int a = 0; a <= 6; a++){ //luping ke kanan
                 System.out.print("* ");
             }
                 System.out.println("");
-        }
+            }
+       }
     }
-    
-}
