8a4f168372603d519dd71e09aa97720335eb41f5
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-11-17 21:23:50 +0800

    menggunakan inputan Scanner Tipe data string dan byte

--- Day_21.java
+++ Day_21.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
          Scanner Futri = new Scanner (System.in);
         
-        System.out.print("masukkan kata : ");
-        String kata = Futri. nextLine();
+        System.out.print("masukkan kata : "); //pendeklarasian kata 
+        String kata = Futri. nextLine();      //untuk memasukkan inputan
         System.out.print("masukkan angka : ");
         byte angka = Futri. nextByte();
         
