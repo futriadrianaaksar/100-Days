502c11c4333048e7bb7359446e7f6f4936e69078
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-11-20 17:03:53 +0800

    Menghitung berat bada ideal

---
+++ Day_43.java
@@ -0,0 +1,31 @@
+package Day43;
+
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Scanner i = new Scanner(System.in);
+        
+        double beratBadan,tinggiBadan,bmi;
+        System.out.print("masukkan berat badan (kg) : ");
+        beratBadan = i.nextDouble();
+        System.out.print("masukkan tinggi badan (m): ");
+        tinggiBadan = i.nextDouble();
+        
+        bmi = beratBadan/(tinggiBadan * tinggiBadan);
+        
+        System.out.printf("BMI ANDA : %.2f\n" , bmi);// %.2f  untuk double dengan 2 angka di belakan koma
+        if (bmi < 18.5) {                            // %.3f  untuk double dengan 3 angka di belakan koma dst
+             System.out.println("Kurang berat badan ");
+        }else if (bmi >= 18.5 && bmi < 25) {
+            System.out.println("Normal ( ideal ) ");
+        }else if (bmi >= 25.0 && bmi < 30) {
+            System.out.println("kelebihan berat badan");
+        }else{
+            System.out.println(" anda Kegemukan ( obesitas )");
+        }
+    }
+    
+}
