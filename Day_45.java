732c57c63bc1c233f56fec9e681f7217b9e3957a
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-11-22 22:35:01 +0800

    membuat persegi
    membuat persegi menggunakan perulangan bersarang

---
+++ Day_45.java
@@ -0,0 +1,15 @@
+package Hari45;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+         //membuat persegi dengan menggunakan perulangan bersarang
+            for(int i = 0; i < 4; i++){ //looping ke bawah
+            for(int a = 0; a < 4; a++){ //looping ke kanan
+                System.out.print("* ");
+            }
+                System.out.println("");
+        }
+    }  
+}
