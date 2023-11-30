c854ade9c505e48f6c7eef0a6d53de7cb5293162
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-11-30 22:26:51 +0800

    membuat program pengiriman data string ke method/fungsi

---
+++ Day_53.java
@@ -0,0 +1,12 @@
+package Day53;
+
+public class Main {
+    
+    public static void Futri(String text){
+        System.out.println(text);
+    }
+    public static void main(String[] args) {
+        String text = "Sehat selalu buat para mentor : ) ";
+        Futri(text);
+    }
+}
