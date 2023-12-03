9b6c56153a44b9af635c3a4646d8b53c30246149
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-12-03 18:49:00 +0800

    Metod fungsi dengan tipe data non primitif

---
+++ Day_56.java
@@ -0,0 +1,17 @@
+package day56;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int angka,angka2;
+        System.out.println("operator penjumlahan");
+        angka = 50;
+        angka2 = 40;
+        System.out.println("\n"+angka +" + "+angka2+" = "+hasilakhir(angka,angka2)+"\n\nAKHIRNYA HAPPY ENDING : )");
+        
+    }
+    public static Integer hasilakhir (int awal, int kedua){
+        int hasil = awal + kedua;
+        return (hasil);
+    }
+}
