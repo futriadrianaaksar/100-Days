d8af4f8e29dd35003b29a28657514d5bce555a37
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-12-04 20:44:21 +0800

    String charAt()
    String charAt() fungsinya memanggil satu karakter pd string sesuai index yg dimasukkan.

---
+++ Day_57.java
@@ -0,0 +1,11 @@
+package day57;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String nama ="futri";
+        char kata = nama.charAt(3);//mengambil karakter ke 2 krn index dimulai dari 0
+        System.out.println("Karakter : "+kata);
+    }
+    
+}
