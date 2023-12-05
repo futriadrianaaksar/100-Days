6d058c408bdd5ab1987e40a81d669fb38b787157
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-12-05 21:25:40 +0800

    konversi huruf string jadi huruf kecil
    misal string isinya : SeLamaT PaGi teMan
    klau di lower case jd : selamat pagi teman

---
+++ Day_58.java
@@ -0,0 +1,10 @@
+package day58;
+
+public class Futri {
+    
+    public static void main(String[] args) {
+        String in= "SeLamaT PaGi teMan";
+        String out= in.toLowerCase();
+        System.out.println(out);
+    }
+}
