2e27f252b3d9b724eefbd1106541724102fe57ad
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-12-06 22:06:39 +0800

    Konversi huruf string ke huruf besar

---
+++ Day_59.java
@@ -0,0 +1,9 @@
+package day59;
+
+public class Futri {
+    
+    public static void main(String[] args) {
+        String in= "selamat malam teman";
+        System.out.println(in.toUpperCase());
+    }
+}
