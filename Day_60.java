b4dd073b5d29f41ab5ec1e0b0e525b7e260d10d2
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-12-07 23:43:20 +0800

    menggunakan fungsi contains pda tipe data string

---
+++ Day_60.java
@@ -0,0 +1,12 @@
+package seratushari;
+public class day60 {
+    public static void main(String[] args) {
+        String kata= "Futri";
+        String abangFutri = "Adriana aksar";
+        if (kata.contains(kata)) {
+            System.out.println("String mengandung contains tersebut");
+        }else{
+          System.out.println("String tidak mengandung contains tersebut");  
+        }
+    }
+}
