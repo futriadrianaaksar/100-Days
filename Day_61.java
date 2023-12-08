b7ef0bab641bbd1f78bd1b96b1d16e739e61d1fe
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-12-08 13:27:46 +0800

    Menggabungkan 3 data

---
+++ Day_61.java
@@ -0,0 +1,12 @@
+package day61;
+
+public class Futri {
+    
+    public static void main(String[] args) {
+        String depan,belakang,anu;
+        depan = "Futri";
+        belakang = "Adriana";
+        anu = "aksar";
+        System.out.println(depan+" "+belakang+" "+anu);
+    }
+}
