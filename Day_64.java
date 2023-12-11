906f1ffd6aee620ac91e4fbef3cc674ac56b8d78
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-12-11 17:48:25 +0800

    buat gambar apapun dri karakter apapun

---
+++ Day_64.java
@@ -0,0 +1,45 @@
+package abangfutri;
+
+public class AbangFutri {
+
+    public static void main(String[] args) {
+        System.err.println("PALUNYA THORRRR!!!!!\nMaaf Kalau Gambarnya Tidak Sempurna : ( \nKarena Kesempurnaan Hanya Milik Allah :)\n");
+        
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 12; j++) {
+                System.out.print(" *");
+            }
+            System.out.println("");
+        }
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 3; j++) {
+                System.out.print(" *");
+            }
+            System.out.println("");
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 8; j++) {
+                System.out.print(" ");  
+            }
+            for (int j = 0; j < 5; j++) {
+                System.out.print(" 0");
+            }
+            System.out.println("");
+        }
+        for (int i = 0; i < 1; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 3; j++) {
+                System.out.print(" *");
+            }
+            System.out.println("");
+        }
+    }
+}
