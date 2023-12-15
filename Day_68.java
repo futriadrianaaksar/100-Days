a12c0c921378d6dbd4e14f204094b3cddde7d2ed
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-12-15 22:50:40 +0800

    Reverse pada String
    Sebelum di balik : irtuF 
    Setelah di balik : Futri

---
+++ Day_68.java
@@ -0,0 +1,10 @@
+package Day68;
+
+public class Day68 {
+    
+    public static void main(String[] args) {
+        String nama = "irtuF";
+        String dibalik= new StringBuilder(nama).reverse().toString();
+        System.out.println("sebelum di balik : " + nama+"\nsetelah di balik : "+dibalik);
+    }
+}
