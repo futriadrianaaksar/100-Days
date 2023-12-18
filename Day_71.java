a2f58e515bf6bd00b031c98d19e12651ae99af14
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-12-18 10:23:11 +0800

    memperbaiki codingan yg dihari 70

---
+++ Day_71.java
@@ -0,0 +1,11 @@
+package Futri;
+public class Bio {
+    String nama,nim;
+    byte umur;
+    
+    public void hasil(){
+        System.out.println("nama : "+nama);
+        System.out.println("nim : "+nim);
+        System.out.println("umur : "+umur);
+    }
+}
