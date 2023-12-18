5430cac0edd915ec5a3581e4fe3592b14d66b596
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-12-18 22:58:00 +0800

    revisi codingan dihari ke 70
    Berisi 2 Program, Materi Object Oriendted Programing class = getter /mengambil dan setter/mengisi

--- Day_71.java
+++ Day_71.java
@@ -1,11 +1,37 @@
-package Futri;
-public class Bio {
-    String nama,nim;
-    byte umur;
+package futri;
+public class Main {
+     public String nama,nim;
+    public int umur;
     
-    public void hasil(){
-        System.out.println("nama : "+nama);
-        System.out.println("nim : "+nim);
-        System.out.println("umur : "+umur);
+    public String getNama(){
+        return nama;
+    }
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+    public String getNim(){
+        return nim;
+    }
+    public void setNim(String nim){
+        this.nim = nim;
+    }
+    public int getUmur(){
+        return umur;
+    }
+    public void setUmur(int umur){
+        this.umur = umur;
+    }
+}
+
+package futri;
+public class dataData {
+    public static void main(String[] args) {
+        Main dataData = new Main();
+        dataData.setNama("futri");
+        System.out.println(dataData.getNama());
+        dataData.setNim("D0223009");
+        System.out.println(dataData.getNim());
+        dataData.setUmur(19);
+        System.out.println(dataData.getUmur());
     }
 }
