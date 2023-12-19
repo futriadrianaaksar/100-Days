7b08259a7f10d1f30bc014d92415310e91f88300
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-12-19 23:12:02 +0800

    membuat fungsi atau method di dalam class

---
+++ Day_72.java
@@ -0,0 +1,21 @@
+package Day72;
+
+public class day72 {
+    
+    public static String mahasiswa(){
+    String nama = "Ana";
+    return nama;
+  
+    }
+    public static void f(String alamat){
+    int umur = 19;
+    System.out.println("Usia\t: "+umur+" tahun\n"+"domisili: "+alamat);
+    
+    }
+    public static void main(String[] args) {
+        int gue = 19;
+        
+        System.out.println("Nama\t: "+mahasiswa());
+        f("tapalang");
+    }
+}
