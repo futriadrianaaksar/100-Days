2e1d50562f95a732e73a21fe680a6bbf52221cad
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-12-23 23:32:02 +0800

    materinya sama, cuman ini pakai tipe data

---
+++ Day_76.java
@@ -0,0 +1,25 @@
+package Day76;
+import java.util.ArrayList;
+import java.util.Vector;
+public class Day76 {
+    
+    public static void main(String[] args) {
+        ArrayList < String> ana = new ArrayList <String>();
+        ana.add("hai");
+        ana.add("aku Futri, biasa di panggil Ana");
+        ana.add("aku suka bermain game");
+        for(String i : ana){
+            System.out.println(i);
+            
+        } System.out.println("___________________");
+        System.out.println("dibawah ini adalah angka favoritku");
+        Vector <Integer> angka =new Vector<Integer>();
+        angka.add(7);
+        angka.add(1);
+        for(Integer a : angka){
+            System.out.println(a);
+            
+        }
+    }
+    
+}
