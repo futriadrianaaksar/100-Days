9514c870ff5ed46e591a65ddab8a5646e80cffcc
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-12-27 20:18:56 +0800

    LinkedList

---
+++ Day_80.java
@@ -0,0 +1,16 @@
+package hari80;
+import java.util.LinkedList;
+public class hari80 {
+    
+    public static void main(String[] args) {
+        LinkedList <String> Nama = new LinkedList <>();
+        Nama.add("Futri");
+        Nama.add("ana");
+        Nama.add("onas");
+        Nama.add("siga");
+        int jumlah = Nama.size();
+        for (String nam : Nama) {
+            System.out.println(nam);
+        }
+        }
+    }
