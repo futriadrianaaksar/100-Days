34f647170c70fd320e33126e07f73e1b4054ed2e
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2023-12-29 07:45:02 +0800

    LinkedList (senarai berkait) =isEmpaty()

---
+++ Day_82.java
@@ -0,0 +1,15 @@
+package Day82;
+import java.util.LinkedList;
+public class Day82 {
+    
+    public static void main(String[] args) {
+        LinkedList <String> ana = new LinkedList <>();
+        ana.add("Hello word");
+        if(ana.isEmpty()){
+            System.out.print("Kosong");
+        }else{
+            System.out.print("tidak Kosong");
+        }
+    }
+    
+}
