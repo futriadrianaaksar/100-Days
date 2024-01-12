3f5e658c6176750378928a2dbd1224420aa52927
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2024-01-12 22:18:13 +0800

    Java GUI menggunakan awt dan swing
    Java GUI menggunakan awt dan swing

---
+++ Day_96.java
@@ -0,0 +1,30 @@
+package Day96;
+import java.awt.Button;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+public class day96 {
+    public static void main(String[] args) {
+        JFrame judul = new JFrame("Data diri");
+        Button klik = new Button ("Klik  saya");
+        klik.setBounds(150, 200, 80, 30);
+        
+        klik.addActionListener( new ActionListener(){
+            public void actionPerformed( ActionEvent e){
+               String nama,alamat,nim;
+               byte umur;
+               nama = "Futri Adriana Aksar";
+               alamat = "Tappalang";
+               nim = "D0223009";
+               umur = 19;
+                System.out.println("Nama\t: "+nama+"\nUmur\t: "+umur+" th"+"\nAlamat\t: "+alamat+"\nNim\t: "+nim);
+            }
+        });
+        judul.setSize(400,500);
+        judul.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        judul.setLocationRelativeTo(null);
+        judul.setLayout(null);
+        judul.setVisible(true);
+        judul.add(klik);
+    }
+}
