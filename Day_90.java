aef14adf3e0fae7f245b278c20f997e3c52f8bd9
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2024-01-14 07:18:30 +0800

    Update Day_90.java

--- Day_90.java
+++ Day_90.java
@@ -8,18 +8,14 @@ public class Day90 {
         a.add("putri");
         a.add("anas");
 
-        // Menampilkan isi LinkedList sebelum penghapusan
         System.out.println("LinkedList sebelum remove: " + a);
-
-        // Menggunakan metode remove() untuk menghapus elemen tertentu
+        
         a.remove();
         System.out.println("LinkedList setelah remove: " + a);
 
-        // Menggunakan metode removeFirst() untuk menghapus elemen di awal
         a.removeFirst();
         System.out.println("LinkedList setelah removeFirst(): " + a);
 
-        // Menggunakan metode removeLast() untuk menghapus elemen di akhir
         a.removeLast();
         System.out.println("LinkedList setelah removeLast(): " + a);
     }
