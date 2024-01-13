e8a36ebaf93283ef81a096a5590c6b591d30e3ed
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2024-01-13 17:01:22 +0800

    membuat method
    membuat method yg didalamnya ada fungsi return value dan parameter bertipe data primitif.

---
+++ Day_97.java
@@ -0,0 +1,14 @@
+package Day97;
+
+public class Day97 {
+    public static int tambah(int a, int b) {
+        int hasil = a + b;
+        return hasil;
+    }
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 3;
+        int result = tambah(x, y);
+        System.out.println("Hasil dari " + x + " + " + y + " adalah " + result);
+    }
+}
