6e06123354af010d411301480e093fc75b30e3a0
futriadrianaaksar <147220465+futriadrianaaksar@users.noreply.github.com>
2024-01-15 21:19:11 +0800

    membuat biodata palsu dengan seluru type data primitif
    Membuat biodata palsu dengan seluru type data primitif

---
+++ Day_99.java
@@ -0,0 +1,17 @@
+package Day99;
+
+public class Day99 {
+    
+    public static void main(String[] args) {
+        int beratBadan = 70;
+        float ipk = 3.52f;
+        char jenisKelamin = 'L';
+        boolean statusMenikah = true;
+        
+        System.out.println("Biodata Palsu \n");
+        System.out.println("Berat Badan   : " + beratBadan + " kg");
+        System.out.println("IPK           : " + ipk);
+        System.out.println("Jenis Kelamin : " + jenisKelamin);
+        System.out.println("Status Menikah: " + statusMenikah);
+    }
+}
